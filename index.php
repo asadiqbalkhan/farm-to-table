0580e91aaf9e6edb4aa4f48d7ccb47394b086ff5
asadiqbalkhan <asadwalker@hotmail.com>
2018-05-11 00:05:53 +0800

    Included Modal for Login -> Included in the main index page for the user.

--- index.php
+++ index.php
@@ -15,14 +15,6 @@
     <source src="vid/farm.ogg" type="video/ogg">
   <h1>Farmable</h1>
   <p>We specialize in farming</p>
-  <!-- <form>
-    <div class="input-group">
-      <input type="email" class="form-control" size="50" placeholder="Email Address" required>
-      <div class="input-group-btn">
-        <button type="button" class="btn btn-danger">C</button>
-      </div>
-    </div>
-  </form> -->
 </div>
 
 <!-- Container (About Section) -->
@@ -35,14 +27,60 @@
       Our products include fruits, vegetables, cage-free eggs, herbs, cheese, grass-fed beef and lamb and pasture-raised chicken all raised without hormones or antibiotics.
       We partner with the very best farmers in Kuching Sarawak that utilize sustainable agriculture and ranching methods to produce the highest quality, most flavorful product found on the market.</p>
       <br>
-      <a href="#contact" ><button  class="btn btn-default btn-lg">Join us</button></a>
+      <!-- TEST STARTS HERE -->
+      <button type="button" class="btn btn-default btn-lg" id="myBtn"><span class="glyphicon glyphicon-log-in"></span> Login</button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header" style="padding:35px 50px;">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+            </div>
+            <div class="modal-body" style="padding:40px 50px;">
+              <form role="form">
+                <img src="img/cheflogo.png" alt="Avatar" class="center">
+                <div class="form-group">
+                  <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+                  <input type="text" class="form-control" id="username" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+                  <input type="text" class="form-control" id="password" placeholder="Enter password">
+                </div>
+                <div class="checkbox">
+                  <label><input type="checkbox" value="" checked>Remember me</label>
+                </div>
+                  <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <p>Not a member? <a href="#">Sign Up</a></p>
+              <p>Forgot <a href="#">Password?</a></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-grain logo"></span>
+    <!-- JS for the modal -->
+    <script>
+    $(document).ready(function(){
+        $("#myBtn").click(function(){
+            $("#myModal").modal();
+        });
+    });
+    </script>
+      <!-- TEST ENDS HERE -->
+      <div class="col-sm-4">
+        <span class="glyphicon glyphicon-grain logo"></span>
+      </div>
     </div>
   </div>
 </div>
-
 <div class="container-fluid bg-grey">
   <div class="row">
     <div class="col-sm-4">

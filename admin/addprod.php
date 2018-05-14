f3f636724a18910ec58e4240af499fda8c57f583
asadiqbalkhan <asadwalker@hotmail.com>
2018-05-15 06:31:45 +0800

    Add product logic implemented.

---
+++ admin/addprod.php
@@ -0,0 +1,19 @@
+<?php
+include_once("connection.php");
+
+$query = "INSERT INTO inventory(`itemCategory`, `itemPrice`, `itemName`, `itemQuantity`)
+  VALUES ('".$_POST["category"]."','".$_POST["price"]."','".$_POST["product"]."','".$_POST["quantity"]."')";
+
+        if (mysqli_query($connection, $query)) {
+             echo "<div>
+                <script>
+                        window.alert('Item have been added successfully');
+                        window.location.href = 'inventory.php?flag=0';
+                </script>
+                </div>";
+            }
+         else {
+            echo "Error: $sql <br />" . mysqli_error($connection);
+        }
+
+?>
